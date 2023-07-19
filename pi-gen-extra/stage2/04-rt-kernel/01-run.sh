26bb6b358cdd0a7edcb7789c92fc67b9a1fd519d
Till Blaha <28341894+tblaha@users.noreply.github.com>
2023-07-19 02:03:46 +0200

    forgot to uncomment

--- pi-gen-extra/stage2/04-rt-kernel/01-run.sh
+++ pi-gen-extra/stage2/04-rt-kernel/01-run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -e
 
 # get sources
-# rm -rf "${STAGE_WORK_DIR}/linux"
-# git clone --depth=1 https://github.com/raspberrypi/linux "${STAGE_WORK_DIR}/linux"
+rm -rf "${STAGE_WORK_DIR}/linux"
+git clone --depth=1 https://github.com/raspberrypi/linux "${STAGE_WORK_DIR}/linux"
 cd ${STAGE_WORK_DIR}/linux
 
 # get and apply RT patch-set. IMPORTANT TODO: do not use hardcoded versions, or git clone only this branch above
-# wget https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.1/patch-6.1.38-rt13-rc1.patch.gz
-# gunzip patch-6.1.38-rt13-rc1.patch.gz
-# cat patch-6.1.38-rt13-rc1.patch | patch -p1
+wget https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.1/patch-6.1.38-rt13-rc1.patch.gz
+gunzip patch-6.1.38-rt13-rc1.patch.gz
+cat patch-6.1.38-rt13-rc1.patch | patch -p1
 
 # configure the kernel
 KERNEL=kernel8
-# make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
-# ./scripts/config --disable CONFIG_VIRTUALIZATION
-# ./scripts/config --enable CONFIG_PREEMPT_RT
-# ./scripts/config --disable CONFIG_RCU_EXPERT
-# ./scripts/config --enable CONFIG_RCU_BOOST
-# ./scripts/config --set-val CONFIG_RCU_BOOST_DELAY 500
+make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+./scripts/config --disable CONFIG_VIRTUALIZATION
+./scripts/config --enable CONFIG_PREEMPT_RT
+./scripts/config --disable CONFIG_RCU_EXPERT
+./scripts/config --enable CONFIG_RCU_BOOST
+./scripts/config --set-val CONFIG_RCU_BOOST_DELAY 500
 
 # cross compile the kernel
-# make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
 
 # install kernel
 env PATH=$PATH make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH="${ROOTFS_DIR}" modules_install
