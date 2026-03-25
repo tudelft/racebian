312769dc3c106348d57c958c552e76521e7e778a
Till Blaha <28341894+tblaha@users.noreply.github.com>
2026-03-25 15:00:07 +0100

    fixes and RT compiler for bookworm on RPi5

--- pi-gen-extra/stage2/04-rt-kernel/01-run.sh
+++ pi-gen-extra/stage2/04-rt-kernel/01-run.sh
@@ -2,27 +2,30 @@
 
 # get sources (latest non-buster kernel according to https://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/)
 rm -rf "${STAGE_WORK_DIR}/linux"
-git clone --depth=1 --branch=stable_20240529 https://github.com/raspberrypi/linux "${STAGE_WORK_DIR}/linux"
+git clone --depth=1 --branch=stable_20250916 https://github.com/raspberrypi/linux "${STAGE_WORK_DIR}/linux"
 cd ${STAGE_WORK_DIR}/linux
 
-# get and apply RT patch.
-KERNEL_VERSION=6.6.31
-RT_VERSION=patches-${KERNEL_VERSION}-rt31
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/older/${RT_VERSION}.tar.gz
-tar xfvz ${RT_VERSION}.tar.gz
-cat ${RT_VERSION} | patch -p1
+# get and apply RT patch. (see kernel Makefile for version)
+# KERNEL_VERSION=6.12.47
+# RT_VERSION=patches-${KERNEL_VERSION}-rt31
+# wget https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/older/${RT_VERSION}.tar.gz
+# tar xfvz ${RT_VERSION}.tar.gz
+# cat ${RT_VERSION} | patch -p1
+
+# since 6.12, the RT patch is now merged into mainline, so we just need to enable it in the config
 
 # configure the kernel
 KERNEL=kernel8
-make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+# make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig # RPi4/RPi400/Zero2W/CM4...
+make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2712_defconfig # RPi5
 ./scripts/config --disable CONFIG_VIRTUALIZATION
 ./scripts/config --enable CONFIG_PREEMPT_RT
 ./scripts/config --disable CONFIG_RCU_EXPERT
 ./scripts/config --enable CONFIG_RCU_BOOST
 ./scripts/config --set-val CONFIG_RCU_BOOST_DELAY 500
-
+ 
 # cross compile the kernel
-make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs bindeb-pkg
 
 # install kernel
 rm -rf "${ROOTFS_DIR}/lib/modules/${KERNEL_VERSION}-v8+" # if not properly cleaned?
